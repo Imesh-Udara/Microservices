34bf81eb0963ec76ac8040fc6fbece3382c4f48c
Imesh <2019cs171@stu.ucsc.lk>
2024-07-26 11:42:23 +0530

    update students and schools

--- school/src/main/java/com/imesh/school/SchoolController.java
+++ school/src/main/java/com/imesh/school/SchoolController.java
@@ -1,8 +1,9 @@
-package com.imesh.student;
+package com.imesh.school;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,18 +11,18 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/students")
 @RequiredArgsConstructor
-public class StudentController {
+public class SchoolController {
 
-    private final StudentService service;
+    private final SchoolService service;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void save(@RequestBody Student student){
-        service.saveStudent(student);
+    public void save(@RequestBody School school){
+        service.saveSchool(school);
     }
 
     @GetMapping
-    public ResponseEntity<List<Student>> findAllStudents(){
-        return ResponseEntity.ok(service.findAllStudents());
+    public ResponseEntity<List<School>> findAllStudents(){
+        return ResponseEntity.ok(service.findAllSchools());
     }
 }
